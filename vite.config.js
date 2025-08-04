a055368c339b895f6a3df6317cbd3f6b55196b26
Matthew Anderson <manderson0182@gmail.com>
2025-08-04 13:31:01 -0400

    Initial home page with some css

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig(({ command }) => ({
+  root: "src/",
+  base: "/",
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html")
+      },
+    },
+  },
+}));
