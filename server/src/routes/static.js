4a10ef56fd9ec936c6165a0fce26db859bb0c2a6
Matthew Anderson <manderson0182@gmail.com>
2025-08-04 14:56:43 -0400

    stuff

--- server/src/routes/static.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const path = require("path");
-
-router.use(express.static(path.join(__dirname, "../client/src/public")));
-
-module.exports = router;
